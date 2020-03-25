8c562197e8a4ac3633a792cbf021924708310c3d
Ellison Patterson <silence@xenogamers.org>
2020-03-25 16:01:58 -0400

    Update SamlSso.php

--- src/Jobs/SamlSso.php
+++ src/Jobs/SamlSso.php
@@ -2,30 +2,15 @@
 
 namespace CodeGreenCreative\SamlIdp\Jobs;
 
-use CodeGreenCreative\SamlIdp\Contracts\SamlContract;
-use CodeGreenCreative\SamlIdp\Events\Assertion as AssertionEvent;
 use CodeGreenCreative\SamlIdp\Traits\PerformsSingleSignOn;
+use CodeGreenCreative\SamlIdp\Traits\SamlParameters;
 use Illuminate\Foundation\Bus\Dispatchable;
-use LightSaml\Binding\BindingFactory;
-use LightSaml\Context\Profile\MessageContext;
-use LightSaml\Credential\KeyHelper;
-use LightSaml\Credential\X509Certificate;
 use LightSaml\Helper;
-use LightSaml\Model\Assertion\Assertion;
-use LightSaml\Model\Assertion\AttributeStatement;
-use LightSaml\Model\Assertion\AudienceRestriction;
-use LightSaml\Model\Assertion\AuthnContext;
-use LightSaml\Model\Assertion\AuthnStatement;
-use LightSaml\Model\Assertion\Conditions;
-use LightSaml\Model\Assertion\EncryptedAssertionWriter;
 use LightSaml\Model\Assertion\Issuer;
 use LightSaml\Model\Assertion\NameID;
-use LightSaml\Model\Assertion\Subject;
-use LightSaml\Model\Assertion\SubjectConfirmation;
-use LightSaml\Model\Assertion\SubjectConfirmationData;
 use LightSaml\Model\Context\DeserializationContext;
-use LightSaml\Model\Protocol\AuthnRequest;
-use LightSaml\Model\Protocol\Response;
+use LightSaml\Model\Protocol\LogoutRequest;
+use LightSaml\Model\Protocol\LogoutResponse;
 use LightSaml\Model\Protocol\Status;
 use LightSaml\Model\Protocol\StatusCode;
 use LightSaml\Model\XmlDSig\SignatureWriter;
@@ -33,135 +18,89 @@ use LightSaml\SamlConstants;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
 
-class SamlSso implements SamlContract
+class SamlSlo
 {
-    use Dispatchable, PerformsSingleSignOn;
+    use Dispatchable, PerformsSingleSignOn, SamlParameters;
+
+    private $sp;
 
     /**
      * [__construct description]
+     * @param [type] $sp [description]
      */
-    public function __construct()
+    public function __construct($sp)
     {
+        $this->sp = $sp;
         $this->init();
     }
 
     /**
-     * Execute the job.
-     *
-     * @return void
+     * [handle description]
+     * @param  [type] $sp [description]
+     * @return [type]     [description]
      */
     public function handle()
     {
-        $deserializationContext = new DeserializationContext;
-        $deserializationContext->getDocument()->loadXML(gzinflate(base64_decode(request('SAMLRequest'))));
-
-        $this->authn_request = new AuthnRequest;
-        $this->authn_request->deserialize($deserializationContext->getDocument()->firstChild, $deserializationContext);
-
         $this->setDestination();
-
-        return $this->response();
+        // We are receiving a Logout Request
+        if ($this->hasSamlRequest()) {
+            $xml = $this->getSamlRequest();
+            $deserializationContext = new DeserializationContext;
+            $deserializationContext->getDocument()->loadXML($xml);
+            // Get the final destination
+            session()->put('RelayState', $this->getSamlRelayState());
+        } elseif ($this->hasSamlResponse()) {
+            $xml = $this->getSamlResponse();
+            $deserializationContext = new DeserializationContext;
+            $deserializationContext->getDocument()->loadXML($xml);
+        }
+        // Send the request to log out
+        return $this->request();
     }
 
+    /**
+     * [response description]
+     * @return [type] [description]
+     */
     public function response()
     {
-        $this->response = (new Response)->setIssuer(new Issuer($this->issuer))
-            ->setStatus(new Status(new StatusCode('urn:oasis:names:tc:SAML:2.0:status:Success')))
+        $this->response = (new LogoutResponse)->setIssuer(new Issuer($this->issuer))
             ->setID(Helper::generateID())
             ->setIssueInstant(new \DateTime)
             ->setDestination($this->destination)
-            ->setInResponseTo($this->authn_request->getId());
-
-        $assertion = new Assertion;
-        $assertion
-            ->setId(Helper::generateID())
-            ->setIssueInstant(new \DateTime)
-            ->setIssuer(new Issuer($this->issuer))
-            ->setSignature(new SignatureWriter($this->certificate, $this->private_key))
-            ->setSubject(
-                (new Subject)
-                    ->setNameID((new NameID(auth()->user()->email, SamlConstants::NAME_ID_FORMAT_EMAIL)))
-                    ->addSubjectConfirmation(
-                        (new SubjectConfirmation)
-                            ->setMethod(SamlConstants::CONFIRMATION_METHOD_BEARER)
-                            ->setSubjectConfirmationData(
-                                (new SubjectConfirmationData())
-                                    ->setInResponseTo($this->authn_request->getId())
-                                    ->setNotOnOrAfter(new \DateTime('+1 MINUTE'))
-                                    ->setRecipient($this->authn_request->getAssertionConsumerServiceURL())
-                            )
-                    )
-            )
-            ->setConditions(
-                (new Conditions)
-                    ->setNotBefore(new \DateTime)
-                    ->setNotOnOrAfter(new \DateTime('+1 MINUTE'))
-                    ->addItem(
-                        new AudienceRestriction([$this->authn_request->getIssuer()->getValue()])
-                    )
-            )
-            ->addItem(
-                (new AuthnStatement)
-                    ->setAuthnInstant(new \DateTime('-10 MINUTE'))
-                    ->setSessionIndex(Helper::generateID())
-                    ->setAuthnContext(
-                        (new AuthnContext)
-                            ->setAuthnContextClassRef(SamlConstants::NAME_ID_FORMAT_UNSPECIFIED)
-                    )
-            );
-
-        $attribute_statement = new AttributeStatement;
-        event(new AssertionEvent($attribute_statement));
-        // Add the attributes to the assertion
-        $assertion->addItem($attribute_statement);
-
-        // Encrypt the assertion
-        if (config('samlidp.encrypt_assertion')) {
-            $this->setSpCertificate();
-
-            $encryptedAssertion = new EncryptedAssertionWriter();
-            $encryptedAssertion->encrypt($assertion, KeyHelper::createPublicKey(
-                (new X509Certificate)->loadPem($this->sp_certificate)
-            ));
-            $this->response->addEncryptedAssertion($encryptedAssertion);
-        } else {
-            $this->response->addAssertion($assertion);
-        }
+            ->setInResponseTo($this->logout_request->getId())
+            ->setStatus(new Status(new StatusCode('urn:oasis:names:tc:SAML:2.0:status:Success')));
 
         if (config('samlidp.messages_signed')) {
             $this->response->setSignature(new SignatureWriter($this->certificate, $this->private_key));
         }
 
-        return $this->send(SamlConstants::BINDING_SAML2_HTTP_POST);
+        return $this->send(SamlConstants::BINDING_SAML2_HTTP_REDIRECT);
     }
 
     /**
-     * [sendSamlRequest description]
-     *
-     * @param  Request $request [description]
-     * @param  User    $user    [description]
-     * @return [type]           [description]
+     * [request description]
+     * @return [type] [description]
      */
-    public function send($binding_type)
+    public function request()
     {
-        $bindingFactory = new BindingFactory;
-        $postBinding = $bindingFactory->create($binding_type);
-        $messageContext = new MessageContext;
-        $messageContext->setMessage($this->response)->asResponse();
-        $message = $messageContext->getMessage();
-        $message->setRelayState(request('RelayState'));
-        $httpResponse = $postBinding->send($messageContext);
+        $this->response = (new LogoutRequest)
+            ->setIssuer(new Issuer($this->issuer))
+            ->setNameID((new NameID(Helper::generateID(), SamlConstants::NAME_ID_FORMAT_TRANSIENT)))
+            ->setID(Helper::generateID())
+            ->setIssueInstant(new \DateTime)
+            ->setDestination($this->destination);
 
-        return $httpResponse->getContent();
+        if (config('samlidp.messages_signed')) {
+            $this->response->setSignature(new SignatureWriter($this->certificate, $this->private_key));
+        }
+
+        return $this->send(SamlConstants::BINDING_SAML2_HTTP_REDIRECT);
     }
 
     private function setDestination()
     {
-        $destination = config(sprintf(
-            'samlidp.sp.%s.destination',
-            $this->getServiceProvider($this->authn_request)
-        ));
-
+        $destination = $this->sp['logout'];
         $queryParams = $this->getQueryParams();
         if (!empty($queryParams)) {
             $destination = Str::finish(url($destination), '?') . Arr::query($queryParams);
@@ -169,13 +108,10 @@ class SamlSso implements SamlContract
 
         $this->destination = $destination;
     }
-
-    private function getQueryParams()
-    {
-        $queryParams = config(sprintf(
-            'samlidp.sp.%s.query_params',
-            $this->getServiceProvider($this->authn_request)
-        ));
+ 
+   private function getQueryParams()
+   {
+        $queryParams = (isset($this->sp['query_params']) ? $this->sp['query_params'] : null);
 
         if (is_null($queryParams)) {
             $queryParams = [
@@ -184,13 +120,5 @@ class SamlSso implements SamlContract
         }
 
         return $queryParams;
-    }
-
-    public function setSpCertificate()
-    {
-        $this->sp_certificate = config(sprintf(
-            'samlidp.sp.%s.certificate',
-            $this->getServiceProvider($this->authn_request)
-        ));
-    }
+   }
 }
